68bbc519ec57ca9bc2a123812b3ee4766199ccfe
unglaublicherdude <github@matthias-simonis.de>
2024-03-03 03:53:02 +0100

    remove the tests folder

--- tests/db-wordpress_test.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress_test;
-GRANT ALL PRIVILEGES ON wordpress_test.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';
-GRANT ALL PRIVILEGES ON wordpress_test.* TO 'wordpress'@'%' IDENTIFIED BY 'wordpress';
-FLUSH PRIVILEGES;
