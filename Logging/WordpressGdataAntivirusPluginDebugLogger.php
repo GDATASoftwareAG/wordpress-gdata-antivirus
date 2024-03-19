4e408e8f0af9236ef63fe3ec7f5f27d008f22ecf
Simonis, Matthias <Matthias.Simonis@gdata.de>
2024-03-19 10:30:34 +0000

    now way to append

--- Logging/WordpressGdataAntivirusPluginDebugLogger.php
+++ Logging/WordpressGdataAntivirusPluginDebugLogger.php
@@ -8,10 +8,7 @@ if (!class_exists('WordpressGdataAntivirusPluginDebugLogger')) {
         public static function Log($message)
         {
             if (defined('WP_DEBUG_LOG') && is_string(WP_DEBUG_LOG)) {
-                global $wp_filesystem;
-                require_once ABSPATH . 'wp-admin/includes/file.php';
-                WP_Filesystem();
-                $wp_filesystem->put_contents(WP_DEBUG_LOG, $message);
+                \file_put_contents(WP_DEBUG_LOG, $message, \FILE_APPEND);
             }
         }
     }
