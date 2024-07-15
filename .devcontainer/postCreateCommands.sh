fbc9041dee5c10d8d3d735ecfa0a66799353ab46
PT-ATA No One <ata-no-one@gdata.de>
2024-07-15 21:40:47 +0000

    devcontainer does setup wordpress

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 sudo apt-get update 
 sudo apt-get install -y bash-completion vim
@@ -8,4 +9,12 @@ sudo bash -c "composer completion bash > /usr/share/bash-completion/completions/
 echo ". /usr/share/bash-completion/bash_completion" >> /home/vscode/.bashrcsource
 
 pwd
-docker compose up
+docker compose up --quiet-pull --wait -d
+
+#docker exec -it wordpress-gdata-antivirus-app-1 bash -c "usermod --shell /bin/bash www-data"
+docker exec -it wordpress-gdata-antivirus-app-1 bash -c "curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
+docker exec -it wordpress-gdata-antivirus-app-1 bash -c "curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
+docker exec -it wordpress-gdata-antivirus-app-1 bash -c "chmod +x wp-cli.phar"
+docker exec -it wordpress-gdata-antivirus-app-1 bash -c "mv wp-cli.phar /usr/local/bin/wp"
+docker exec -it wordpress-gdata-antivirus-app-1 bash -c "wp core install --url=http://localhost:8080 --title=testsite --admin_user=admin --admin_email=vaas@gda
+ta.de --admin_password=admin"
