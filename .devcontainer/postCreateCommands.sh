95d094d8038bc6ed3f34a54d16bcf09c73dba5a7
PT-ATA No One <ata-no-one@gdata.de>
2024-07-16 09:35:53 +0000

    start wordpress upon container start

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -9,12 +9,17 @@ sudo bash -c "composer completion bash > /usr/share/bash-completion/completions/
 echo ". /usr/share/bash-completion/bash_completion" >> /home/vscode/.bashrcsource
 
 pwd
-docker compose up --quiet-pull --wait -d
+docker compose rm --force --stop --volumes
+docker compose up --quiet-pull --wait -d --force-recreate --renew-anon-volumes --remove-orphans
 
 #docker exec -it wordpress-gdata-antivirus-app-1 bash -c "usermod --shell /bin/bash www-data"
 docker exec -it wordpress-gdata-antivirus-app-1 bash -c "curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
 docker exec -it wordpress-gdata-antivirus-app-1 bash -c "curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
 docker exec -it wordpress-gdata-antivirus-app-1 bash -c "chmod +x wp-cli.phar"
 docker exec -it wordpress-gdata-antivirus-app-1 bash -c "mv wp-cli.phar /usr/local/bin/wp"
-docker exec -it wordpress-gdata-antivirus-app-1 bash -c "wp core install --url=http://localhost:8080 --title=testsite --admin_user=admin --admin_email=vaas@gda
-ta.de --admin_password=admin"
+until docker exec -it --user www-data wordpress-gdata-antivirus-app-1 bash -c "wp core install --url=http://localhost:8080 --title=testsite --admin_user=admin --admin_email=vaas@gdata.de --admin_password=admin"
+do
+    echo "waiting for nextcloud to be installed"
+    sleep 2
+done
+composer install
