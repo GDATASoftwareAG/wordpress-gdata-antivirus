3c4c3833d36cdbdd949640f30c2fbf7b5b3fcb2f
PT-ATA No One <ata-no-one@gdata.de>
2024-09-24 11:15:50 +0000

    phpcs

--- tests/unittests/Infrastructure/NoopFindingsQuery.php
+++ tests/unittests/Infrastructure/NoopFindingsQuery.php
@@ -23,12 +23,9 @@ class NoopFindingsQuery implements IFindingsQuery {
         return 0;
     }
 
-    public function validate(): void {
-    }
+    public function validate(): void {}
 
-    public function create(): void {
-    }
+    public function create(): void {}
 
-    public function remove(): void {
-    }
+    public function remove(): void {}
 }
