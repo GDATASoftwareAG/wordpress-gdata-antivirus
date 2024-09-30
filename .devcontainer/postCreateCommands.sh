204a5ebdcbf9523656b22db9742d3f94ab67263e
PT-ATA No One <ata-no-one@gdata.de>
2024-09-30 07:06:06 +0000

    install php-scoper

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -6,7 +6,10 @@ sudo apt-get install -y bash-completion vim iputils-ping telnet
 
 sudo bash -c "docker completion bash > /usr/share/bash-completion/completions/docker"
 sudo bash -c "composer completion bash > /usr/share/bash-completion/completions/composer"
-
 echo ". /usr/share/bash-completion/bash_completion" >> /home/vscode/.bashrcsource
+
+composer global require humbug/php-scoper
+/home/vscode/.composer/vendor/bin/php-scoper completion bash >> /home/vscode/.bash_completion
+echo 'export PATH=/home/vscode/.composer/vendor/bin/:$PATH' >>~/.bashrc
 
 source .devcontainer/configureWordPress.sh
