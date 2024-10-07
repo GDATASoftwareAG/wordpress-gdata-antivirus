3d11cd5b8de15607aa5c1cb512237502a90910c6
PT-ATA No One <ata-no-one@gdata.de>
2024-10-07 11:58:36 +0000

    we are not in a devcontainer

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -ex
 
+composer global require humbug/php-scoper
+$(composer config home)/bin/php-scoper completion bash >> /home/vscode/.bash_completion
+echo "export PATH=$(composer config home)/vendor/bin/:$PATH" >> ~/.bashrc
+
+sudo cp memory.ini /usr/local/etc/php/conf.d/memory.ini
+
+if [[ "$IS_CI" == "true" ]]; then
+    exit 0
+fi
+
 sudo bash -c "docker completion bash > /usr/share/bash-completion/completions/docker"
 sudo bash -c "composer completion bash > /usr/share/bash-completion/completions/composer"
 echo ". /usr/share/bash-completion/bash_completion" >> /home/vscode/.bashrcsource
 
-composer global require humbug/php-scoper
-/home/vscode/.composer/vendor/bin/php-scoper completion bash >> /home/vscode/.bash_completion
-echo 'export PATH=/home/vscode/.composer/vendor/bin/:$PATH' >>~/.bashrc
-
-sudo cp memory.ini /usr/local/etc/php/conf.d/memory.ini
 
 source .devcontainer/configureWordPress.sh
